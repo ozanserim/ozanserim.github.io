08340e6a37a220e507adb3e277bf1e1ab03bcf0a
Ozan Serim <ozan@Ozans-Yosemite.hf>
2016-02-26 20:39:53 -0800

    hacking snapshot function

--- repro/repro.js
+++ repro/repro.js
@@ -4,20 +4,20 @@
 function repro(){
     new OverlayWebWindow({
         title: 'Google',
-        source: "https://ozanserim.github.io/repro.html",
+        source: "https://ozanserim.github.io/repro/repro.html",
         width: 900,
         height: 700,
         visible: true
     });
 
-    /*
+
     if (key.text === "s") {
         if (ctrlIsPressed === true) {
             noteString = "Snapshot taken.";
             createNotification(noteString, NotificationType.SNAPSHOT);
         }
     }
-    */
+
     //Script.stop();
 }
 repro();
