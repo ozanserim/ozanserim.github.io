8bf173a29090a3be99777e1084ee232934c7172d
Ozan Serim <ozan@Ozans-Yosemite.hf>
2016-02-27 16:04:21 -0800

    added eventBridge function

--- repro/repro.js
+++ repro/repro.js
@@ -2,14 +2,16 @@
  * Created by ozan on 2/25/16.
  */
 function repro(){
-    new OverlayWebWindow({
+    var window new OverlayWebWindow({
         title: 'Google',
         source: "https://ozanserim.github.io/repro/repro.html",
         width: 900,
         height: 700,
         visible: true
     });
+    //var window = new OverlayWebWindow(…);
 
+    window.eventBridge.emitScriptEvent(JSON.stringify("DATA_TO_SEND"));
     //Script.stop();
 }
 repro();
