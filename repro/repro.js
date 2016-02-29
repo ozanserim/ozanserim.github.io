6a94a9f0f771daf8f4c7fe4babb6f6fa97ba7ebb
Ozan Serim <ozan@Ozans-Yosemite.hf>
2016-02-28 18:05:20 -0800

    added eventBridge function

--- repro/repro.js
+++ repro/repro.js
@@ -2,7 +2,7 @@
  * Created by ozan on 2/25/16.
  */
 function repro(){
-    var webView = new WebWindow('Repro1', "https://ozanserim.github.io/repro/repro.html", 800, 600, false);
+    var webView = new WebWindow('Repro2', "https://ozanserim.github.io/repro/repro.html", 800, 600, false);
     webView.setVisible(true);
     webView.eventBridge.emitScriptEvent(JSON.stringify("test_data"));
     //Script.stop();
