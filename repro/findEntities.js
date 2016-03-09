3882d8a786e08d1def07b367f8c0ddf299fff9b9
Ozan Serim <ozan@Ozans-Yosemite.hf>
2016-03-09 15:19:00 -0800

    updated repro

--- repro/findEntities.js
+++ repro/findEntities.js
@@ -1,15 +1,15 @@
-function randomProperites(){
-    var position = Vec3.sum(MyAvatar.position,{x:Math.random(),y:Math.random(), z:Math.random()});
-    var color = {red:Math.random()*200,green:Math.random()*100,blue:Math.random()*200};
-
-    var properties = {
-        type: "Sphere",
-        position: position,
-        color: color
-    };
-//print("Created some random colors" + JSON.stringify(color) + "\n at some random location" + JSON.stringify(position));
-    return properties;
-}
+//function randomProperites(){
+//    var position = Vec3.sum(MyAvatar.position,{x:Math.random(),y:Math.random(), z:Math.random()});
+//    var color = {red:Math.random()*200,green:Math.random()*100,blue:Math.random()*200};
+//
+//    var properties = {
+//        type: "Sphere",
+//        position: position,
+//        color: color
+//    };
+////print("Created some random colors" + JSON.stringify(color) + "\n at some random location" + JSON.stringify(position));
+//    return properties;
+//}
 
 //Creates a bunch of entities
 //for (i = 0; i < 20; i++){
@@ -19,16 +19,18 @@ function randomProperites(){
 
 // find the closes one and make it white
 entList= Entities.findEntities(MyAvatar.position, 100.0)
-//print(entList.length);
+print("entity length = " + entList.length);
 
 for( i = 0; i < entList.length; i++){
     //Entities.editEntity(arrayFound[i], {color: {red:200, blue:200, green:200}});
+    var modelTyp  = JSON.stringify(Entities.getEntityProperties(entList[i]).type);
     var modelUrl  = JSON.stringify(Entities.getEntityProperties(entList[i]).modelURL);
     var modelPos = JSON.stringify(Entities.getEntityProperties(entList[i]).position);
-    if (modelUrl != null)
-    {
-        print("modelUrl: "+ modelUrl);
-        print("modelPos: "+ modelPos);
+    //if (modelUrl != null)
+    //{
+    print("modelType: "+ modelTyp);
+    print("modelUrl: "+ modelUrl);
+    print("modelPos: "+ modelPos);
 
-    }
+    //}
 }
--- repro/repro.js
+++ repro/repro.js
@@ -2,6 +2,7 @@
  * Created by ozan on 2/25/16.
  */
 
+//create window and link to html page
 var reproWindow = new OverlayWebWindow({
     title: 'Repro3',
     source: "file:///Users/ozan/WebstormProjects/ozanserim/repro/repro.html",
@@ -9,18 +10,44 @@ var reproWindow = new OverlayWebWindow({
     height: 600,
     visible: false
 });
-
 reproWindow.setVisible(true);
 reproWindow.raise();
 
+var myName = "Ozan";
+//var posText = JSON.stringify(MyAvatar.position);
+var myAvatar = MyAvatar.skeletonModelURL;
+var myLocation = location.href;
+var myPosition = JSON.stringify(MyAvatar.position);
+myPosition = myPosition.replace("{", "");
+myPosition = myPosition.replace("}", "");
+myPosition = myPosition.replace(/["]+/g, '');
+var message = '{"avatar": "'+myAvatar+'", "location": "'+myLocation+'", "position": "'+myPosition+'"}';
+//var message = '{"avatar": "'+myAvatar+'"}';
+//var message = '{"location": "'+myLocation+'"}';
+//var message = '{"position": "'+myPosition+'"}';
+print ("ozan message = " + message);
+
+getEntities();
+
+function getEntities(){
+    entList= Entities.findEntities(MyAvatar.position, 100.0)
+    //print("entity length = " + entList.length);
+
+    for( i = 0; i < entList.length; i++){
+        var modelTyp  = JSON.stringify(Entities.getEntityProperties(entList[i]).type);
+        var modelUrl  = JSON.stringify(Entities.getEntityProperties(entList[i]).modelURL);
+        var modelPos = JSON.stringify(Entities.getEntityProperties(entList[i]).position);
+
+        var entityInfo = modelTyp + ", "+modelUrl+", "+modelPos;
+        print ("entityInfo = " + entityInfo);
+        //print("modelType: "+ modelTyp);
+        //print("modelUrl: "+ modelUrl);
+        //print("modelPos: "+ modelPos);
+    }
+}
+//create eventBridge
 Script.setTimeout(function(){
-    reproWindow.eventBridge.emitScriptEvent("test_data");
-
-
-
-    //start doing stuff here
-
-
-
+    reproWindow.eventBridge.emitScriptEvent(message);
+    //do stuff here
 },3000)
 
