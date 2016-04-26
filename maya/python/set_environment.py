b5f49b40b588a6f278339ab7b19b9677937e5881
Ozan Serim <ozan@Ozans-Yosemite.hf>
2016-04-26 13:14:31 -0700

    added skeleton template

--- maya/python/set_environment.py
+++ maya/python/set_environment.py
@@ -1,4 +1,4 @@
 #SETUP ENV
 
 import pymel.core as pm
-pm.mel.eval(' setProject "/Users/ozan/Documents/assets/anim/standard_anims" ')
+pm.mel.eval(' setProject "/Users/ozan/Documents/assets/anim/avatars/hifi_default_avatars/priscilla" ')
